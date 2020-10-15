d5680e56cff59ec9a41dd1d3f5bc261200da5211
Loïc Dessaules <loic.dessaules@gmail.com>
2020-10-15 14:27:54 +0200

    Add asynchonous activity

--- app/src/main/java/activities/AsynchonousActivity.java
+++ app/src/main/java/activities/AsynchonousActivity.java
@@ -3,8 +3,18 @@ package activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import ch.heigvd.iict.sym.lab.R;
+import ch.heigvd.iict.sym.lab.comm.CommunicationEventListener;
+import ch.heigvd.iict.sym.lab.comm.SymComManager;
 
 public class AsynchonousActivity extends AppCompatActivity {
 
@@ -12,5 +22,68 @@ public class AsynchonousActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asynchonous);
+
+        // Set the actionbar title
+        getSupportActionBar().setTitle(R.string.asynchrone_actionbar_title);
+
+        final RelativeLayout backgroundLoader = findViewById(R.id.asynchronous_layoutLoader);
+        final ProgressBar loader =  findViewById(R.id.asynchronous_loader);
+        final TextView tvDataReceived = findViewById(R.id.asynchronous_tvDataReceived);
+
+        // Create the Handler to be able to modify the UIthread when receive specific message
+        final Handler handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                hideLoader(backgroundLoader, loader);
+                tvDataReceived.setText(msg.getData().getString("FROM_SERVER"));
+                Log.d("AsynchonousActivity", msg.getData().getString("FROM_SERVER"));
+            }
+        };
+
+        // Our thread to send the request, fetch the response and send it through the handler asynchronously
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                displayLoader(backgroundLoader, loader);
+
+                SymComManager symComManager = new SymComManager();
+                symComManager.setCommunicationEventListener(new CommunicationEventListener() {
+                    @Override
+                    public boolean handleServerResponse(String response) {
+                        Message msg = handler.obtainMessage();
+                        Bundle b = new Bundle();
+                        b.putString("FROM_SERVER", response);
+                        msg.setData(b);
+                        handler.sendMessage(msg);
+                        return true;
+                    }
+                });
+
+                symComManager.sendRequest("http://sym.iict.ch/rest/txt", "Hey !");
+            }
+        });
+
+        thread.start();
+
+    }
+
+    /**
+     * Display the loader
+     * @param backgroundLoader RelativeLayout background
+     * @param loader ProgressBar loader
+     */
+    private void displayLoader(RelativeLayout backgroundLoader, ProgressBar loader) {
+        backgroundLoader.setVisibility(View.VISIBLE);
+        loader.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Hide the loader
+     * @param backgroundLoader RelativeLayout background
+     * @param loader ProgressBar loader
+     */
+    private void hideLoader(RelativeLayout backgroundLoader, ProgressBar loader) {
+        loader.setVisibility(View.GONE);
+        backgroundLoader.setVisibility(View.GONE);
     }
 }
--- app/src/main/java/ch/heigvd/iict/sym/lab/comm/SymComManager.java
+++ app/src/main/java/ch/heigvd/iict/sym/lab/comm/SymComManager.java
@@ -1,11 +1,49 @@
 package ch.heigvd.iict.sym.lab.comm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class SymComManager {
 
     private CommunicationEventListener communicationEventListener = null;
 
-    public void sendRequest(String url, String request) {
-		//TODO to implement
+    public void sendRequest(String strURL, String message) {
+        try {
+            // Init the connection
+            URL url = new URL (strURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+
+            // SetParams
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "text/plain; utf-8");
+            con.setRequestProperty("X-Network:", "CSD"); // For the moment hardcode the bad quality
+            con.setDoOutput(true);
+
+            // Write the output
+            OutputStream os = con.getOutputStream();
+            byte[] input = message.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+
+            // Read the response
+            InputStreamReader in = new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(in);
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim() + "\n");
+            }
+
+            // Callback call to notify the response
+            communicationEventListener.handleServerResponse(response.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void setCommunicationEventListener(CommunicationEventListener communicationEventListener) {
