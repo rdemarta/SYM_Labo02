2a662fa2064bf45e571bab7ca27de593a50f7d26
Loïc Dessaules <loic.dessaules@gmail.com>
2020-10-29 11:45:05 +0100

    Add json activity design

--- app/src/main/java/ch/heigvd/iict/sym/lab/activities/JsonActivity.java
+++ app/src/main/java/ch/heigvd/iict/sym/lab/activities/JsonActivity.java
@@ -3,6 +3,8 @@ package ch.heigvd.iict.sym.lab.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import ch.heigvd.iict.sym.lab.R;
 
@@ -12,5 +14,13 @@ public class JsonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json);
+
+        // Set the actionbar title
+        getSupportActionBar().setTitle(R.string.json_actionbar_title);
+
+        final EditText etLastName = findViewById(R.id.json_etLastName);
+        final EditText etfirstName = findViewById(R.id.json_etFirstName);
+        final EditText etAge = findViewById(R.id.json_etAge);
+        final Button btnSend = findViewById(R.id.json_btnSend);
     }
 }
