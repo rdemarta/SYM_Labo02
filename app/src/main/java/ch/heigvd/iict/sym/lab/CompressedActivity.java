3655aae236806795dab23e26c93dfa439bb64024
Robin Demarta <r.demarta@outlook.com>
2020-11-05 10:47:20 +0100

    Base activity

---
+++ app/src/main/java/ch/heigvd/iict/sym/lab/CompressedActivity.java
@@ -0,0 +1,14 @@
+package ch.heigvd.iict.sym.lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompressedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compressed);
+    }
+}
